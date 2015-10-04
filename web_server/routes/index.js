cbd4337970e80e4c959c9960d19e1809cb94c232
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2015-10-04 13:21:47 -0400

    add

--- web_server/routes/index.js
+++ web_server/routes/index.js
@@ -4,18 +4,25 @@
  */
 
 var twilio = require('twilio');
-
+var kinectdb = require("../model/kinectdb");
 var client = new twilio.RestClient('AC0662afad9cf5f3753a00b71e5b0ad975', '73deaec2c6121bea06685b15c6580fce');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
-
-  	
+  
 	client.sms.messages.create({
 	    to:'+1 514-574-8677',
-	    from:'1 647-931-1254',
+	    from:'+1 647-931-1254',
 	    body: JSON.stringify(req.body)
 	}, function(error, message) {
+			var newEntry = new kinectdb.Entry({value: JSON.stringify(req.body)});
+			newEntry.save(function(err){
+				if (err) {
+					console.log("Error saving Entry to DB: " + err);
+					res.send(404);
+				}
+				res.send(200);
+			});
 	    // The HTTP request to Twilio will run asynchronously. This callback
 	    // function will be called when a response is received from Twilio
 	    // The "error" variable will contain error information, if any.
